75dbb94c84ec6c87f4d406e53ccf0008b880d622
galihanggara68 <galihanggara68@gmail.com>
2021-08-27 00:03:43 +0700

    Initial

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using ConTestWeb.Util;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ConTestWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public WeatherForecastController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public List<string> GetUsers()
+        {
+            Connector con = new Connector(_config.GetValue<string>("ConString"));
+            List<string> ret = new List<string>();
+            SqlConnection connection = con.GetConnection();
+            connection.Open();
+            try
+            {
+                SqlCommand query = new SqlCommand("select username from users", connection);
+                SqlDataReader reader = query.ExecuteReader();
+                while (reader.Read())
+                {
+                    ret.Add(reader["username"].ToString());
+                }
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            connection.Close();
+            return ret;
+        }
+    }
+}
---
+++ Util/Connector.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace ConTestWeb.Util
+{
+    public class Connector
+    {
+        private SqlConnection connection;
+
+        public Connector(string conString)
+        {
+            connection = new SqlConnection(conString);
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return this.connection;
+        }
+    }
+}
